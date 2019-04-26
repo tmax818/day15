ce10e5d00fbd990367c6528b58086e44e82fd153
Tyler Maxwell <tylermaxwell661@gmail.com>
2019-04-26 10:50:10 -0700

    added links final

--- src/App.js
+++ src/App.js
@@ -7,16 +7,23 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>This is</p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="http://www.tylermaxwell.co/portfolio"
           target="_blank"
           rel="noopener noreferrer"
         >
           Day 15
+        </a>
+        <p>of my</p>
+        <a
+          className="App-link"
+          href="https://medium.com/rubypythoncjs/180-websites-in-180-days-14a3c18f4984"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          180 day challenge
         </a>
       </header>
     </div>
